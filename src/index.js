ee13e48b2974bb8bc621b7bc5c821a7901c48ccd
zhengfeng.yao <zhengfeng.yao@tongdun.cn>
2017-05-03 17:26:28 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,127 @@
+/**
+ * @Author: Zhengfeng.Yao <yzf>
+ * @Date:   2017-05-03 15:53:34
+ * @Last modified by:   yzf
+ * @Last modified time: 2017-05-03 15:55:49
+ */
+
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+import is from './is'
+
+function loadTheme(theme) {
+  if (is.String(theme)) {
+    return require(theme)
+  }
+  if (is.Object(theme)) {
+    return theme
+  }
+  return null
+}
+
+module.exports = (config, options) => {
+  const cssLoader = {
+    loader: `css-loader${is.Object(options) && options.target === 'node' ? '/locals' : ''}`,
+    options: {
+      sourceMap: is.Object(options) && !!options.sourceMap,
+      modules: true,
+      localIdentName: '[local]',
+      minimize: is.Object(options) && options.target !== 'node' && !!options.minimize,
+      discardComments: { removeAll: true }
+    }
+  }
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      plugins: () => {
+        if (is.Object(options) && is.Array(options.postCss)) {
+          return options.postCss
+        }
+        return [
+          require('postcss-nested')(),
+          require('pixrem')(),
+          require('autoprefixer')(is.Object(options) && is.Object(options.autoprefixer) ? options.autoprefixer : {
+            browsers: [
+              'Chrome >= 35',
+              'Firefox >= 31',
+              'Explorer >= 9',
+              'Opera >= 12',
+              'Safari >= 7.1'
+            ]
+          }),
+          require('postcss-flexibility')(),
+          require('postcss-discard-duplicates')()
+        ]
+      }
+    }
+  }
+  const lessLoader = {
+    loader: 'less-loader',
+    options: {
+      sourceMap: is.Object(options) && !!options.sourceMap,
+      modifyVars: is.Object(options) ? loadTheme(options.theme) : null
+    }
+  }
+
+  if (is.Object(options) && options.target === 'node') {
+    config.add('rule.less', {
+      test: /\.less$/,
+      use: [
+        cssLoader,
+        postcssLoader,
+        lessLoader
+      ]
+    })
+    config.add('rule.css', {
+      test: /\.css$/,
+      use: [
+        cssLoader,
+        postcssLoader
+      ]
+    })
+    return
+  }
+  if (is.Object(options) && !!options.extractCss) {
+    config.add('plugin.ExtractText', new ExtractTextPlugin(is.String(options.extractCss) ? options.extractCss : '[name].css'))
+    config.add('rule.less', {
+      test: /\.less$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          cssLoader,
+          postcssLoader,
+          lessLoader
+        ]
+      })
+    })
+    config.add('rule.css', {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          cssLoader,
+          postcssLoader
+        ]
+      })
+    })
+    return
+  }
+
+  config.add('rule.less', {
+    test: /\.less$/,
+    use: [
+      'style-loader',
+      cssLoader,
+      postcssLoader,
+      lessLoader
+    ]
+  })
+  config.add('rule.css', {
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      cssLoader,
+      postcssLoader
+    ]
+  })
+}
---
+++ test/index-spec.js
@@ -0,0 +1,62 @@
+/**
+ * @Author: Zhengfeng.Yao <yzf>
+ * @Date:   2017-05-03 16:32:00
+ * @Last modified by:   yzf
+ * @Last modified time: 2017-05-03 16:32:02
+ */
+
+import * as chai from 'chai'
+import extend from '../src/index'
+
+const expect = chai.expect
+
+class Config {
+  constructor() {
+    this.config = {}
+  }
+
+  add = (key, value) => {
+    this.config[key] = value
+  }
+}
+
+describe('index', () => {
+  it('test no options', () => {
+    const configObj = new Config()
+    extend(configObj)
+    expect(configObj.config['rule.css']).to.not.be.undefined
+    expect(configObj.config['rule.less'].use).to.include.members(['style-loader'])
+  })
+
+  it('test node options', () => {
+    const configObj = new Config()
+    extend(configObj, {
+      target: 'node',
+      sourceMap: true,
+      minimize: true,
+      theme: {
+        '@font-size': '14px'
+      },
+      postCss: [
+        'test'
+      ]
+    })
+    expect(configObj.config['rule.less'].use[0].loader).to.equal('css-loader/locals')
+    expect(configObj.config['rule.less'].use[2].options.sourceMap).to.equal(true)
+    expect(configObj.config['rule.less'].use[1].options.plugins()).to.includes.members(['test'])
+  })
+
+  it('test extractCss options', () => {
+    const configObj = new Config()
+    extend(configObj, {
+      extractCss: true,
+      postCss: true
+    })
+    expect(configObj.config['plugin.ExtractText']).to.not.be.undefined
+    expect(configObj.config['rule.css'].use.slice(-1)[0].options.plugins().length).to.equal(5)
+    extend(configObj, {
+      extractCss: 'test.js'
+    })
+    expect(configObj.config['plugin.ExtractText'].filename).to.equal('test.js')
+  })
+})
